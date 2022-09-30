559abd622128075c4863bdf20353dbdd9cc722ef
William <95254357+glassyeyedfish@users.noreply.github.com>
2022-09-29 23:19:25 -0400

    Update phase-2.sh

--- phase-2.sh
+++ phase-2.sh
@@ -4,7 +4,7 @@ mkfs.ext4 /dev/sda2
 mkswap /dev/sda1
 mount /dev/sda2 /mnt
 swapon /dev/sda1
-pacstrap /mnt base linux linux-firmware git neovim tmux networkmanager man-db man-pages grub
+pacstrap /mnt base linux linux-firmware git neovim tmux networkmanager man-db man-pages grub sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo Now you can chroot with:
