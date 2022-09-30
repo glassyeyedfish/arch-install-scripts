e1ef164cbd3918d92b2fddc36c6af04ba299378b
William <95254357+glassyeyedfish@users.noreply.github.com>
2022-09-29 23:12:30 -0400

    Update phase-3.sh

--- phase-3.sh
+++ phase-3.sh
@@ -5,3 +5,6 @@ echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "archbox" > /etc/hostname
+echo "127.0.0.1        localhost" > /etc/hosts
+echo "::1              localhost" >> /etc/hosts
+echo "127.0.1.1        archbox" >> /etc/hosts
